a6ee68280a40e9eec835014becce728d8fbeb0e5
abberg <andrewbberg@gmail.com>
2015-05-30 22:42:34 -0400

    tweaks

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # atm_0003
 
 Undulating cube wave
+
+Based on [F L U X by Candas Sisman](http://www.csismn.com/filter/Motion/F-L-U-X)
+
+![F L U X](http://payload.cargocollective.com/1/2/68300/813909/3_o.jpg)
+
--- src/scripts/sketch.js
+++ src/scripts/sketch.js
@@ -13,26 +13,28 @@
 
 			init = function(){
 				
-				var geometry,
-					material,
-					mesh,
-					i,
-					numBoxes = 200,
+				var numBoxes = 200,
 					boxWidth = 0.05,
 					boxDepth = 2,
+					geometry = new THREE.BoxGeometry( boxWidth, 1, boxDepth ),
+					material = new THREE.MeshPhongMaterial( { color: 0xFF3300, shininess: 100 } ),
+					mesh,
+					geometry2 = new THREE.BoxGeometry( 0.02, 1, 1.25 ),
+					mesh2,
+					edges,
+					i,
 					container,
 					model,
 					previousModel,
 					keyLight = new THREE.DirectionalLight( 0xffffff ),
 					bounceLight = new THREE.DirectionalLight( 0xffffff, 0.3 ),
+					backLight = new THREE.SpotLight( 0xffffff, 0.5, 0, Math.PI/3, 10.0),
 					debugCanvas;
 
 				container = new THREE.Object3D();
 
 				for(i = 0; i < numBoxes; i++){
 
-					geometry = new THREE.BoxGeometry( boxWidth, 1, boxDepth ),
-					material = new THREE.MeshPhongMaterial( { color: 0xFF3300, shininess: 100 } ),
 					mesh = new THREE.Mesh( geometry, material ),
 					container.add(mesh);
 
@@ -41,10 +43,24 @@
 					model.rotation.x = i * 0.04 ;
 					previousModel = model.clone();
 
+					mesh2 = new THREE.Mesh( geometry2 );
+					mesh2.visible = false;
+					edges = new THREE.EdgesHelper( mesh2, 0x990000 );
+					container.add(mesh2);
+					mesh.add(edges);
+					mesh2.position.y = -0.8;
+					mesh2.position.z = 0.25;
+
 					cubes.push({mesh:mesh, model:model, previousModel:previousModel});
 
 				}
 				scene.add( container );
+
+				geometry = new THREE.PlaneBufferGeometry( 100, 100, 1 );
+				material = new THREE.MeshPhongMaterial( { color: 0x4c6467, shininess:1} ),
+				mesh = new THREE.Mesh( geometry, material );
+				mesh.position.z = -5;
+				scene.add( mesh );
 
 				scene.add(keyLight);
 				keyLight.position.set( 1, 1, 0.5 );
@@ -52,10 +68,13 @@
 				scene.add(bounceLight);
 				bounceLight.position.set( 1, -1, 1 );
 
+				scene.add(backLight);
+				backLight.position.set( 0, -5, 0 );
+				backLight.target = mesh;
+				
 				renderer.setClearColor(0x2D4445);
 
 				camera.position.z = 3;
-
 
 				debugCanvas = document.createElement('canvas');
 				debugCanvas.style.position = 'absolute';
@@ -64,28 +83,28 @@
 				debugCanvas.style.zIndex = '1';
 				document.body.appendChild(debugCanvas);
 				debugContext = debugCanvas.getContext('2d');
-
 				debugCanvas.style.display = 'none';
 			
 			},
 			
 			update = function(timestep){
 				
-				var noiseSpeed = 0.02,
+				var noiseSpeed = 0.015,
 					noiseScale = 0.2,
-					rotationVelocity = 0.01,
+					rotationVelocity = 0.02,
 					noise,
 					index = 0;
 
 				cubes.forEach(function(cube){
 
 					noise = simplex.noise2D(cube.model.position.x * noiseScale, noiseY * noiseScale);
+					cube.model.position.y = noise;
 					noise = ((noise + 1) * 0.5);
 
 					cube.model.clone(cube.previousModel);
-					cube.model.rotation.x -= rotationVelocity * noise;
-					cube.model.scale.y = noise;
-					cube.model.scale.z = 1-noise;
+					cube.model.rotation.x -= rotationVelocity + ( 0.01 * noise );
+					cube.model.scale.y = 0.1 + Math.pow( 1 - noise, 4 );
+					cube.model.scale.z = 0.1 + Math.pow( 1 - noise, 4 ) * 3;
 
 					var gray = parseInt(noise * 255);
                    	debugContext.fillStyle = 'rgb('+gray+', '+gray+', '+gray+')';
